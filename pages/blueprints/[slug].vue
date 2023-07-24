17ebac84b85ccb16a2b89951a951ec97fee42b2f
Laura Caroline Ribeiro de Melo <laura@laura-melo.com>
2023-07-24 13:36:46 +0200

    Add tailwindcss

--- pages/blueprints/[slug].vue
+++ pages/blueprints/[slug].vue
@@ -1,12 +1,12 @@
 <template>
-  <h1>{{ data?.title }}</h1>
-  <div>
-    <div>{{ data?.type }}</div>
-    <div v-for="tag in data?.tags" :key="tag">{{ tag }}</div>
+  <h1 class="text-2xl font-bold mb-4">{{ data?.title }}</h1>
+  <div class="flex gap-1 mb-2">
+    <div class="bg-green-100 rounded-full px-2">{{ data?.type }}</div>
+    <div v-for="tag in data?.tags" :key="tag" class="bg-blue-100 rounded-full px-2">{{ tag }}</div>
   </div>
-  <div>Minecraft Version: {{ data?.minecraftVersion }}</div>
-  <p>{{ data?.description }}</p>
-  <youtube-player :youtubeLink="data?.youtubeLink" />
+  <div><strong>Minecraft Version:</strong> {{ data?.minecraftVersion }}</div>
+  <p class="my-4">{{ data?.description }}</p>
+  <youtube-player v-if="data?.youtubeLink" :youtubeLink="data?.youtubeLink" />
 </template>
 
 <script setup lang="ts">
